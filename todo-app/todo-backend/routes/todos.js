4b05b1ba672d965d164148a7fdc84359e3757853
nikogenix <nikogenix@gmail.com>
2023-06-12 12:39:05 +0300

    Completing exercises

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,49 +1,58 @@
-const express = require('express');
-const { Todo } = require('../mongo')
+const express = require("express");
+const { Todo } = require("../mongo");
 const router = express.Router();
 
 /* GET todos listing. */
-router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
-  res.send(todos);
+router.get("/", async (_, res) => {
+	const todos = await Todo.find({});
+	res.send(todos);
 });
 
 /* POST todo to listing. */
-router.post('/', async (req, res) => {
-  const todo = await Todo.create({
-    text: req.body.text,
-    done: false
-  })
-  res.send(todo);
+router.post("/", async (req, res) => {
+	const todo = await Todo.create({
+		text: req.body.text,
+		done: false,
+	});
+	res.send(todo);
 });
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+	const { id } = req.params;
+	req.todo = await Todo.findById(id);
+	if (!req.todo) return res.sendStatus(404);
 
-  next()
-}
+	next();
+};
 
 /* DELETE todo. */
-singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
+singleRouter.delete("/", async (req, res) => {
+	await req.todo.delete();
+	res.sendStatus(200);
 });
 
 /* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+singleRouter.get("/", async (req, res) => {
+	res.send(req.todo);
 });
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+singleRouter.put("/", async (req, res) => {
+	const { text, done } = req.body;
+	try {
+		const todo = await Todo.findByIdAndUpdate(req.todo.id, { text, done }, { new: true });
+		if (!todo) {
+			return res.sendStatus(404);
+		}
+		res.send(todo);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(500);
+	}
 });
 
-router.use('/:id', findByIdMiddleware, singleRouter)
-
+router.use("/:id", findByIdMiddleware, singleRouter);
 
 module.exports = router;
